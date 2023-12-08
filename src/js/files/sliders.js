047cff7c8e9373b771685ac1597bbeb497e0c397
avroratomsk <avroratomsk@yandex.ru>
2023-12-08 15:45:40 +0700

    feat: doctors slider index page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -107,6 +107,85 @@ function initSliders() {
       }
     });
   }
+  if (document.querySelector('.index-doctors__slider')) { // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.index-doctors__slider', { // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination, Autoplay],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 4,
+      spaceBetween: 31,
+      autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+
+      // Эффекты
+      // effect: 'fade',
+      autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+      },
+
+      // Пагинация
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: false,
+      },
+
+      // Скроллбар
+      /*
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+      */
+
+      // Кнопки "влево/вправо"
+      navigation: {
+        prevEl: '.index-button-prev',
+        nextEl: '.index-button-next',
+      },
+
+      // navigation: {
+      //   prevEl: '.swiper-button-prev',
+      //   nextEl: '.swiper-button-next',
+      // },
+
+      // Брейкпоинты
+      // breakpoints: {
+      // 	320: {
+      // 		slidesPerView: 1,
+      // 		spaceBetween: 20,
+      // 		autoHeight: true,
+      // 	},
+      // 	485: {
+      // 		slidesPerView: 2,
+      // 		spaceBetween: 20,
+      // 	},
+      // 	992: {
+      // 		slidesPerView: 3,
+      // 		spaceBetween: 20,
+      // 	},
+      // 	1268: {
+      // 		slidesPerView: 3,
+      // 		spaceBetween: 30,
+      // 	},
+      // },
+      // События
+      on: {
+
+      }
+    });
+  }
 }
 
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
